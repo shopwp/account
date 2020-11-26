94e5368ed23fcf8fa71a61862705fc56fee9f9e3
Andrew Robbins <arobbins@simpleblend.net>
2020-11-25 22:51:02 -0600

    Added header

---
+++ src/components/nav-main/index.jsx
@@ -0,0 +1,14 @@
+function NavMain() {
+  return (
+    <nav>
+      <a href='https://wpshop.io/try' class='btn'>
+        Start for free
+      </a>
+      <a href='https://wpshop.io/try' class='link'>
+        Log In
+      </a>
+    </nav>
+  );
+}
+
+export default NavMain;
