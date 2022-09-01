c4bcd449005ceb597462f0b50a939129f2b47c77
Andrew Robbins <hello@wpshop.io>
2022-09-01 11:28:41 -0500

    Added redirects again

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import './App.css';
-import { AccountProvider } from './components/account/_state/provider';
-import Bootstrap from './components/bootstrap';
+import './App.css'
+import { AccountProvider } from './components/account/_state/provider'
+import Bootstrap from './components/bootstrap'
+
+window.location.replace('https://wpshop.io/account')
 
 function App() {
-  return (
-    <AccountProvider>
-      <Bootstrap />
-    </AccountProvider>
-  );
+	return (
+		<AccountProvider>
+			<Bootstrap />
+		</AccountProvider>
+	)
 }
 
-export default App;
+export default App
